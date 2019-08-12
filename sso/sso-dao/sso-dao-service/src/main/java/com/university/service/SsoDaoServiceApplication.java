5a58a54dfcf4b8e1ee975983531c9f4df96889be
LuciferMS <2039176261@qq.com>
2019-08-13 00:54:32 +0800

    Initial commit

---
+++ sso/sso-dao/sso-dao-service/src/main/java/com/university/service/SsoDaoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.university.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoDaoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsoDaoServiceApplication.class, args);
+    }
+
+}
---
+++ sso/sso-service/sso-service-service/src/main/java/com/university/sso/service/service/SsoServiceServiceApplication.java
@@ -0,0 +1,16 @@
+package com.university.sso.service.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Elliot
+ */
+@SpringBootApplication
+public class SsoServiceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsoServiceServiceApplication.class, args);
+    }
+
+}
