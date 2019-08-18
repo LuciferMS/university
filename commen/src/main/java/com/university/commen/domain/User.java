c60f30e736ed81431a40207824544ede012dfa56
LuciferMS <2039176261@qq.com>
2019-08-18 16:05:33 +0800

    '构建查询体系'

---
+++ commen/src/main/java/com/university/commen/domain/User.java
@@ -0,0 +1,8 @@
+/**
+ * @program: university
+ * @description: user
+ * @author: Elliot
+ * @create: 2019-08-18 10:59
+ **/
+public class User {
+}
---
+++ sso/sso-commen/src/main/java/com/university/sso/commen/domain/Token.java
@@ -0,0 +1,8 @@
+/**
+ * @program: university
+ * @description: tooken
+ * @author: Elliot
+ * @create: 2019-08-18 11:08
+ **/
+public class Token {
+}
