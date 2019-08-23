4c0aab86a1402c96212020d883b55bb20a84b4da
LuciferMS <2039176261@qq.com>
2019-08-23 15:50:31 +0800

    '分布式服务调用跑通了...'

---
+++ sso/sso-dao/src/test/java/com/university/sso/dao/test/spring/ApplicationContextTest.java
@@ -0,0 +1,15 @@
+package com.university.sso.dao.test.spring;
+
+/**
+ * @program: university
+ * @description: 测试bean容器
+ * @author: Elliot
+ * @create: 2019-08-23 11:36
+ **/
+public class ApplicationContextTest {
+
+    public void initApplicationContextTest(){
+
+    }
+
+}
